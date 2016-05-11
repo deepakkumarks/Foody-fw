9a467237576cea1d405081ce0f5d10193dc8e438
kishorekumaru <kishorekumaru@gmail.com>
2016-05-11 22:41:58 +1000

    added login form

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,2 +1,20 @@
 // jshint devel:true
-console.log('\'Allo \'Allo!');
+var foody = angular.module('foody', ['ngResource']);
+
+foody.controller('mainCntrl', ['$scope', function($scope){
+	$scope.heading = "Login";
+	$scope.login = $scope.form = {};
+
+	$scope.checkValidation = function(){
+		$scope.reset();
+		if($scope.form.email == "admin" && $scope.form.pwd == "admin"){
+			$scope.login.success =true;
+		}else {
+			$scope.login.failed =true;
+		}
+	}
+
+	$scope.reset = function(){
+			$scope.login.success  = $scope.login.failed =false;
+	}
+}]);
